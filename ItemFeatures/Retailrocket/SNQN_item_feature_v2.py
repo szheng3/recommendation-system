3eb001afb924395e7363d788e49b1a8e33b4e2f4
Sept <sszheng3@gmail.com>
2023-04-30 16:39:40 -0400

    filter

--- ItemFeatures/Retailrocket/SNQN_item_feature_v2.py
+++ ItemFeatures/Retailrocket/SNQN_item_feature_v2.py
@@ -308,10 +308,14 @@ class QNetwork:
             print("feature embedding shape")
             print(self.feature_embedding.shape)
             dot_product = tf.matmul(self.states_hidden, tf.transpose(self.feature_embedding[:, :, :-1], perm=[0, 2, 1]))
+            print("dot product shape")
+            print(dot_product.shape)
+
 
             # Add the bias term from feature_embedding[:, -1]
             self.phi_prime = tf.reshape(dot_product, [-1, 1]) + self.feature_embedding[:, :, -1]
-
+            print("phi_prime shape")
+            print(self.phi_prime.shape)
             # CHANGES: Calculate phi'
 
 
